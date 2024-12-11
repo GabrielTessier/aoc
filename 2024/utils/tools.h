377425aea1fc2185e9d99ebef0028ba2001fdf27
Gabriel <gabriel.tessier45@gmail.com>
2024-12-11 18:38:22 +0100

    modif tools.h

--- 2024/utils/tools.h
+++ 2024/utils/tools.h
@@ -6,7 +6,7 @@ int int_comparator(const void* first, const void* second);
 int abs(const int a);
 void* map_tab(void* tab, size_t nb_elem, size_t size_old, void* (*map_fnc)(void*), size_t size_new);
 void* atoi_void(void* str);
-char** split(char* str, char del, int* nb_sub);
+char** split(char* str, char del, int* nb_sub_ret);
 char** read_file_to_array(char* filename, int* size_ret);
 
 #endif
